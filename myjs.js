b4774f1ddf67b5c385e9d31e636c9f397c8a51b5
OlenaKos <48287055+OlenaKos@users.noreply.github.com>
2020-01-16 13:39:29 +0200

    Add files via upload

--- myjs.js
+++ myjs.js
@@ -1,15 +1,23 @@
 function CalculateCalories(obj)
 {
-var age = parseInt(obj.txtAge.value);
-alert(age);
 var isMan = obj.sexMan.checked;
 alert(isMan);
-var weight = parseInt(obj.txtWeight.value);
-var height = parseInt(obj.txtHeight.value);
 var activity = obj.lstActivity.value;
-alert(weight);
-alert(height);
-alert(activity);
+var isFormula1 = obj.formula1.checked;
+var isResCal = obj.resCal.checked;
+var isHeightS = obj.heightS.checked;
+var isWeightK = obj.weightK.checked;
+var age = parseInt(obj.txtAge.value);
+var weight = parseInt(obj.txtWeight.value);
+if (isWeightK == false)
+{
+weight *= 0.453592;
+}
+var height = parseInt(obj.txtHeight.value);
+if (isHeightS == false)
+{
+height *= 30.48;
+}
 
 var activityCoef = 0.0;
 switch(activity){
@@ -32,27 +40,37 @@ case 'veryLow':
 }
 alert(activityCoef);
 
-var isFormula1 = obj.formula1.checked;
-var isResCal = obj.resCal.checked;
 
 var res = 0;
 
 if (isFormula1 == true)
 {
-if (isMan == true)
+	if (isMan == true)
+	{
+ 		res = (10 * weight + 6,25 * height - 5 * age + 5) * (activityCoef);
+	}
+	else
+	{
+ 		res = (10 * weight + 6,25 * height - 5 * age - 161) * (activityCoef);
+	}
+}
+else
 {
- res = 10 * weight + 6,25 * height - 5 * age + 5;
+	if (isMan == true)
+	{
+ 		res = (88,36 + (13,4 * weight) + 4,8 * height - 5,7 * age) * (activityCoef);
+	}
+	else
+	{
+ 		res = (44,76 + (9,2 * weight) + 3,1 * height - 4,3 * age) * (activityCoef);
+
+	}
+}
+
+if (isResCal  == false)
+{
+res *= 4,1868;
+}
 alert("res = " + res)
-}
-else
-{
-
-}
-
-}
-else
-{
-
-}
 
 }
